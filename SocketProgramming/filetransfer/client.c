184e5e4eeb72883ef34e6ee0decf56e845d0f730
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2024-06-30 14:58:03 +0530

    Create client.c

---
+++ SocketProgramming/filetransfer/client.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define port 12340
+#define buff_size 256
+FILE *fp;
+int main()
+{
+  char buff[buff_size];
+  char filename[20];
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in server_address;
+
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(port);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+
+  if(bind(sockfd, (struct sockaddr*) &server_address, sizeof(server_address))==-1)
+  {
+    perror("bind");
+  }
+  
+  int connfd = connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address));
+  
+  recv(sockfd, filename, sizeof(filename), 0);
+  printf(filename);
+  fp = fopen(filename, "r");
+  if(fp == NULL)
+  {
+    perror("fopen");
+  }
+  else
+  {
+    while (fgets(buff, sizeof(buff), fp)!= NULL)
+      {
+        send(sockfd, buff, sizeof(buff), 0);
+      }
+    fclose(fp);
+  }
+  close(sockfd);
+}
