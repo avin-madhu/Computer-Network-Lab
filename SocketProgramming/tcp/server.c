ba9e9d1a17a1a10a213ce96b48f6a27d81505528
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2024-07-07 12:45:45 +0530

    Update server.c

--- SocketProgramming/tcp/server.c
+++ SocketProgramming/tcp/server.c
@@ -4,59 +4,63 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 2001
+#define PORT 12345
 #define BUFFER_SIZE 256
-
-void error(const char *msg)
-{
-  perror(msg);
-  exit(1);
-}
 
 int main()
 {
-
-  struct sockaddr_in server_address, client_address;
   char BUFFER[BUFFER_SIZE];
-  // make a socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  // initialize the server address
+  if(sockfd == -1)
+  {
+    perror("Socket");
+  }
+  struct sockaddr_in server_address, client_address;
+  memset(&server_address, 0, sizeof(server_address));
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(PORT);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
-  // bind 
-  int binres = bind(sockfd, (struct sockaddr*) 
-  &server_address, sizeof(server_address));
+  // bind the socket address and port address
+  int bindcheck = bind(sockfd, (struct sockaddr*) &server_address, sizeof(server_address));
+  if(bindcheck == -1)
+  {
+    perror("Bind");
+  }
 
-  // listen 
+  // listen for the connection from the client
   listen(sockfd, 5);
-  
 
   socklen_t addr_size = sizeof(client_address);
 
-  int connfd = accept(sockfd, (struct sockaddr*) &client_address, &addr_size);
+  //accepts the connection from the client
+  int newsockfd = accept(sockfd, (struct sockaddr*) &client_address, &addr_size);
 
   while(1)
     {
-      // recieve message from the client
-      bzero(BUFFER, BUFFER_SIZE);
-      read(connfd, BUFFER, sizeof(BUFFER));
+      int recvcheck = recv(newsockfd,BUFFER, BUFFER_SIZE, 0);
+      if(recvcheck == -1)
+      {
+        perror("Recv");
+      }
       printf("Client: %s", BUFFER);
 
-      // send message to the client 
       printf("Server: ");
-      fgets(BUFFER, sizeof(BUFFER), stdin);
-      write(connfd, BUFFER, strlen(BUFFER));
-
-      if(strncmp(BUFFER, "exit", 4)==0)
-      {
-        break;
-      }
+      fgets(BUFFER, BUFFER_SIZE, stdin);
+      send(newsockfd,BUFFER, BUFFER_SIZE, 0);
     }
-
   close(sockfd);
-
+  close(newsockfd);
   return 0;
 }
+
+
+/*
+----------------
+| Extra Notes  |
+----------------
+
+ # Perror() --> basically checks for any error and prints it out on to the 
+   terminal with the given parameter string as prefix.
+
+*/
