31d84bc07cc6ac8ae9dcf392ffebf0a4747cb874
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2024-07-07 13:01:32 +0530

    Update client.c

--- SocketProgramming/udp/client.c
+++ SocketProgramming/udp/client.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
-
-#define PORT 2003
-#define BUFFER_SIZE 1024
-
+#include <arpa/inet.h>
+#define PORT 12347
+#define BUFFER_SIZE 256
 int main()
 {
   char BUFFER[BUFFER_SIZE];
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-  struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(PORT);
-  server_address.sin_addr.s_addr = INADDR_ANY;
-
-  socklen_t server_addr = sizeof(server_address);
-
+  if(sockfd == -1)
+  {
+    perror("Socket");
+  }
+  struct sockaddr_in client_address;
+  memset(&client_address, 0, sizeof(client_address));
+  client_address.sin_family = AF_INET;
+  client_address.sin_port = htons(PORT);
+  client_address.sin_addr.s_addr = INADDR_ANY;
+  socklen_t addr_size = sizeof(client_address);
   while(1)
     {
       printf("Client: ");
       fgets(BUFFER, BUFFER_SIZE, stdin);
-      sendto(sockfd, BUFFER, BUFFER_SIZE, 0, (struct sockaddr*)&server_address, sizeof(server_address));
+      sendto(sockfd, BUFFER, BUFFER_SIZE, 0, (struct sockaddr*) &client_address, sizeof(client_address));
 
-      int rec = recvfrom(sockfd, BUFFER, BUFFER_SIZE, 0, NULL, NULL);
-      if (rec<0)
+      int recvcheck = recvfrom(sockfd,BUFFER, BUFFER_SIZE, 0, NULL, NULL);
+      if(recvcheck == -1)
       {
-        printf("error in recieving data");
+        perror("Recv");
       }
       printf("Server: %s", BUFFER);
-    }
 
+    }
+  close(sockfd);
   return 0;
 }
